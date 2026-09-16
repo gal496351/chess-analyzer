ab40961d54624df8700f3bf754cc085163bacb49
gal496351 <gallevy2002@gmail.com>
2026-09-16 13:16:10 +0300

    Scaffold Vite React TypeScript frontend with chessboard

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Chessboard } from "react-chessboard";
+
+function App() {
+  return (
+    <div style={{ width: "500px", margin: "40px auto" }}>
+      <h1>Chess Analyzer</h1>
+            <Chessboard options={{}} />
+    </div>
+  );
+}
+
+export default App;
